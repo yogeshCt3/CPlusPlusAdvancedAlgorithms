8757aa00b726e87529d6b703ec1e5ac1c244480c
योगेश छेत्रि <36565350+yogeshCt3@users.noreply.github.com>
2019-06-26 09:35:10 +0530

    updates

--- FastMath.hpp
+++ FastMath.hpp
@@ -1,44 +1,41 @@
 #ifndef FASTMATH_HPP
-#define FASEMATH_HPP
+#define FASTMATH_HPP
 
-#include <iostream>
-using ulli = unsigned long long int;
+typedef unsigned long long int  ulli;
+
 using namespace std;
-inline void fastExponentialWithModulo(ulli _base, ulli _exp, const ulli _mod){
-	ulli _ans = 1;
-	while(_exp){
-		if(_exp & 1){ 	//~~> exponential power is odd
-			_ans *= _base;
-			if ( _ans >= _mod ) 
-				_ans %= _mod;	//~~> if term increases the mod then replace with the remainder 
-		}
-		_base *= _base;
-		if( _base >= _mod )
-			_base %= _mod;	//~~> if term increases the mod then replace with the remainder
-		_exp >>= 1; 	//~~> divide the exponential power by 2
-		
-	}	
-	cout << _ans << '\n';
-}
 
-
-inline void fastExponential(ulli _base, ulli _exp){
-	ulli _ans = 1;
-	while(_exp){
-		if(_exp & 1){ 	//~~> exponential power is odd
+inline void fastExp(ulli &_ans, ulli _base, ulli _exp){
+	//	to compute _base ^ _exp	
+	// 	Binary Exponentation O ( log _exp ) 
+	_ans = 1;
+	while (_exp){
+		if(_exp & 1){
 			_ans *= _base;
 		}
 		_base *= _base;
-		_exp >>= 1; 	//~~> divide the exponential power by 2
-		
-	}	
-	cout << _ans << '\n';
-}
+		_exp >>= 1;				
+	}		
+}                          
+
+
+inline void fastExpWithMod(ulli &_ans, ulli _base, ulli _exp, ulli _mod){
+	//	to compute (_base ^ _exp) % _mod	
+	// 	Binary Exponentation O ( log _exp ) 
+	_ans = 1;
+	while (_exp){
+		if(_exp & 1){
+			_ans *= _base;
+			if(_ans >= _mod)
+				_ans %= _mod;
+		}
+		_base *= _base;
+		if(_base >= _mod)
+			_base %= _mod;
+		_exp >>= 1;				
+	}		
+}                          
 
 
 #endif
-
-
-
-
 
